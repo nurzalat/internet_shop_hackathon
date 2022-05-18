7a1b57f8931173ad9e873a19724e7f30fc83a898
admins <gainazarova>
2022-05-18 21:39:10 +0600

    product app implemented pagination filter applied

--- product/views.py
+++ product/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
+from product.models import Product, Category, Comment
+from product.serializers import ProductSerializer, CategorySerializer
+from product import serializers
 
-# Create your views here.
+from rest_framework.pagination import PageNumberPagination
+from django.utils import timezone
+from datetime import timedelta
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Q
+
+
+class StandardPaginationClass(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
+class ProductViewSet(ModelViewSet):
+    class Meta:
+        model = Product
+        fields = '__all__'
+    queryset = Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+    pagination_class = StandardPaginationClass
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        days_count = int(self.request.query_params.get('day', 0))
+        if days_count > 0:
+            start_date = timezone.now() - timedelta(days=days_count)
+            queryset = queryset.filter(created_at__gte=start_date)
+        return queryset
+
+    @action(detail=False, methods=['get'])
+    def search(self, request, pk=None):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q))
+        serializer = ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # api/v1/products/<id>/comments/
+    @action(['GET'], detail=True)
+    def comments(self, request, pk):
+        product = self.get_object()
+        comments = product.comments.all()
+        serializer = serializers.CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+class CategoryView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        print("PERFORM CREATE")
+        print(self.request.user)
+        return serializer.save(owner=self.request.user)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAuthor,)
+
+
+
+
+
